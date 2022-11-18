d9e4fccf2a57208a4aaefcf4f5b41f05e79c2c98
tejas <tejasshah@gmail.com>
2022-11-18 20:54:39 +0530

    res header

---
+++ src/com/controller/ResponseHeaderLoginServlet.java
@@ -0,0 +1,23 @@
+package com.controller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/ResponseHeaderLoginServlet")
+public class ResponseHeaderLoginServlet extends HttpServlet {
+
+	protected void service(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setHeader("authToken", "thisismyheader");
+		// no data
+		response.sendRedirect("ResHeaderHome.jsp");
+
+	}
+}
